8adf40fa3c7da44600201b4850e44f56525e3742
Ricardo8421 <jricardogomeztagle@gmail.com>
2023-05-25 12:02:52 -0600

    php para json de profesores y materias

--- conexion.php
+++ conexion.php
@@ -5,6 +5,7 @@ $c = "n0m3l0";
 $b = "cuestionarios";
 
 $xd = new mysqli($s, $u, $c, $b);
+mysqli_set_charset($xd,'utf8');
 
 if($xd->connect_error){
     die("Hubo un error al conectarse a la base de datos");
