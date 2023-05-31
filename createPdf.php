c1ed874e0c0b8a2b4b71e041a55cfdfc81dbfb40
Jose Monroy <mnry7999@gmail.com>
2023-05-30 22:08:13 -0600

    Exportacion a archivos Pdf

---
+++ createPdf.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'libraries/dompdf/autoload.inc.php';
+echo "Ya jala";
+
+use Dompdf\Dompdf;
+
+$json = obtenerJson();
+$data = json_decode($json, true);
+
+//obteneer el html de plantillaPdf.php
+ob_start();
+include "plantillaPdf.php";
+$html = ob_get_clean();
+
+
+
+$dompdf = new Dompdf();
+$dompdf->loadHtml($html);
+$dompdf->render();
+
+$nombreArchivo = 'mi_archivo.pdf';
+$dompdf->stream($nombreArchivo, ['Attachment' => true]);
+
+function obtenerJson()
+{
+    return '{"nombre": "John Doe", "edad": 30}';
+}
+?>
