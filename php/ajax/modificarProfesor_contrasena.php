b664887e25f29b8b37782f5a82efa209868e278e
Ricardo8421 <jricardogomeztagle@gmail.com>
2023-06-24 17:59:59 -0600

    fixed #6

--- php/ajax/modificarProfesor_contrasena.php
+++ php/ajax/modificarProfesor_contrasena.php
@@ -6,23 +6,26 @@ session_start();
 
 $b=false;
 
-if(isset($_SESSION["usuario"]) && isset($_POST["contrasenaAntigua"]) && isset($_POST["contrasenaNueva"])){
+if(isset($_SESSION["usuario"]) && isset($_POST["contrasenaConfirmacion"]) && isset($_POST["contrasenaAntigua"]) && isset($_POST["contrasenaNueva"])){
     $usuario = $_SESSION["usuario"];
     $contraVieja = $_POST["contrasenaAntigua"];
     $contraAntigua = $_POST["contrasenaNueva"];
-    $query = sprintf("SELECT id FROM usuario WHERE `login`='%s' AND pass='%s'",
-        $mysql->real_escape_string($usuario),
-        $mysql->real_escape_string($contraVieja));
-    $d = $mysql->query($query);
-    if($d->num_rows > 0){
-        $f=$d->fetch_assoc();
-        $query=sprintf("UPDATE usuario SET pass='%s' WHERE id=%d",
-            $mysql->real_escape_string($contraAntigua),
-            $mysql->real_escape_string($f["id"]));
-        $mysql->query($query);
-        if($mysql->affected_rows > 0){
-            $r["msg"]="Sí jaló paps";
-            $b=true;
+    $confirmacion = $_POST["contrasenaConfirmacion"];
+    if($confirmacion==$contraAntigua){
+        $query = sprintf("SELECT id FROM usuario WHERE `login`='%s' AND pass='%s'",
+            $mysql->real_escape_string($usuario),
+            $mysql->real_escape_string($contraVieja));
+        $d = $mysql->query($query);
+        if($d->num_rows > 0){
+            $f=$d->fetch_assoc();
+            $query=sprintf("UPDATE usuario SET pass='%s' WHERE id=%d",
+                $mysql->real_escape_string($contraAntigua),
+                $mysql->real_escape_string($f["id"]));
+            $mysql->query($query);
+            if($mysql->affected_rows > 0){
+                $r["msg"]="Sí jaló paps";
+                $b=true;
+            }
         }
     }
 }
