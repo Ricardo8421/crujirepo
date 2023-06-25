c5000060b46cc2e8ba8f29e80bb1f9f8ebb8577b
OscarJuJi <ojuarezj1800@alumno.ipn.mx>
2023-06-24 19:48:28 -0600

    Correción academias con acento
    se corrigen las materias en las seleccion de selects con acento

--- js/both_js2.js
+++ js/both_js2.js
@@ -4,120 +4,131 @@ fetch("php/ajax/datosMaterias")
 		var datos = data;
 		// Obtener la etiqueta select
 
-		$(document).ready(function() {
+		$(document).ready(function () {
 			// Manejar el cambio en el select
-				// Hacer una solicitud AJAX a tu script PHP
-				async function peticion(seleccionado) {
-					let response= await $.ajax({
-					url: "php/utils/academia_usuario.php",
+			// Hacer una solicitud AJAX a tu script PHP
+			async function peticion(seleccionado) {
+				let response = await $.ajax({
+					url: "php/ajax/datosAcademiasUsuario.php",
 					method: "POST",
 					data: {},
-					success: function(resultado) {
-						let rsp=((resultado));
+					success: function (resultado) {
+						let rsp = ((resultado));
 
-						
-function dividirString(inputString) {
-	// Ignorar los primeros 4 caracteres
-	inputString = inputString.slice(4);
-	
-	// Dividir el string en partes usando el delimitador "
-	var partes = inputString.split('"');
-	
-	// Filtrar las partes vacías
-	partes = partes.filter(function(parte) {
-	  return parte !== "";
-	});
-	
-	// Retornar el resultado como un array de strings
-	return partes;
-  }
-  
+
+						/*function dividirString(inputString) {
+							// Encontrar el índice del primer '[' en el string
+							var startIndex = inputString.indexOf('[');
 						  
-						  // Ejemplo de uso:
-						 
-						  const resultArray = dividirString(rsp);
+							// Si no se encuentra el '[' en el string, retornar un array vacío
+							if (startIndex === -1) {
+							  return [];
+							}
+						  
+							// Ignorar los caracteres antes del '['
+							inputString = inputString.slice(startIndex);
+						  
+							// Dividir el string en partes usando el delimitador "
+							var partes = inputString.split('"');
+						  
+							// Filtrar las partes vacías
+							partes = partes.filter(function (parte) {
+							  return parte !== "";
+							});
+						  
+							// Retornar el resultado como un array de strings
+							return partes;
+						  }
+						  */
+						
 
-									// Agregar el evento change a la etiqueta select
-
-  			
-			// Obtener la academia seleccionada
-			var objetosFiltrados =[];
-			for(let o=0;o<(resultArray.length);o++){
-
-			var academiaSeleccionada =String(resultArray[o]);
-			//console.log(rsp);
-			
-			// Filtrar los objetos del JSON por la academia seleccionada
-			if(academiaSeleccionada!=""&&academiaSeleccionada!="]"&&academiaSeleccionada!="["&&academiaSeleccionada!=","){
-				console.log(academiaSeleccionada);
-			objetosFiltrados.push( datos.filter(function (objeto) {
-				return objeto.Academia == academiaSeleccionada;
-			}))
-					console.log(objetosFiltrados)}
-				}
-			var selectsMaterias_q = document.querySelectorAll("#materia");
-			let breakMaterias = document.querySelectorAll("#break");
-			let labelMaterias = document.querySelectorAll("#materia_label");
-
-			for (var i = 1; i < (labelMaterias.length + 1); i++) {
-				let previous_divs = document.querySelectorAll("#materias_div" + i);
+						const resultArray = rsp[0].academias.split(",");
+						console.log(resultArray);
+						
+						// Agregar el evento change a la etiqueta select
 
 
-				previous_divs[0].parentNode.removeChild(previous_divs[0]);
-			}
+						// Obtener la academia seleccionada
+						var objetosFiltrados = [];
+						for (let o = 0; o < (resultArray.length); o++) {
 
-			for (let i = 1; i < 5; i++) {
-				selectsMaterias_q = document.querySelectorAll("#materia");
+							var academiaSeleccionada = String(resultArray[o]);
+							//console.log(rsp);
 
-				var materia_div = document.createElement("div");
-				materia_div.id = "materias_div" + i;
-				materia_div.name = "materias_div";
-				materia_div.className = "container container-formulario";
-				if (i == 1) { materia_div.style.display = "block" }
-				else materia_div.style.display = "none"
-				materia_div.innerHTML = `
+							// Filtrar los objetos del JSON por la academia seleccionada
+							if (academiaSeleccionada != "" && academiaSeleccionada != "]" && academiaSeleccionada != "[" && academiaSeleccionada != ",") {
+								console.log(academiaSeleccionada);
+								objetosFiltrados.push(datos.filter(function (objeto) {
+									console.log(objeto);
+									return objeto.Academia == academiaSeleccionada;
+								}))
+								console.log(objetosFiltrados)
+							}
+						}
+						var selectsMaterias_q = document.querySelectorAll("#materia");
+						let breakMaterias = document.querySelectorAll("#break");
+						let labelMaterias = document.querySelectorAll("#materia_label");
+
+						for (var i = 1; i < (labelMaterias.length + 1); i++) {
+							let previous_divs = document.querySelectorAll("#materias_div" + i);
+
+
+							previous_divs[0].parentNode.removeChild(previous_divs[0]);
+						}
+
+						for (let i = 1; i < 5; i++) {
+							selectsMaterias_q = document.querySelectorAll("#materia");
+
+							var materia_div = document.createElement("div");
+							materia_div.id = "materias_div" + i;
+							materia_div.name = "materias_div";
+							materia_div.className = "container container-formulario";
+							if (i == 1) { materia_div.style.display = "block" }
+							else materia_div.style.display = "none"
+							materia_div.innerHTML = `
         <br id="break">
         <label for="materia" id="materia_label">Materia ${i}</label>
         <br id="break">
                 `;
-				let nombre="materia" + i;
-				// Crear la nueva etiqueta select
-				var selectMaterias = document.createElement("select");
+							let nombre = "materia" + i;
+							// Crear la nueva etiqueta select
+							var selectMaterias = document.createElement("select");
 
-				selectMaterias.className = "form-control-dark dropdown-menu dropdown-menu-dark d-block position-static mx-0 border-0 shadow w-220px ";
-				selectMaterias.ariaLabel = "Default select example";
-				selectMaterias.style = "background-color: var(--background-color); color: var( --contrast-dark-color);";
-				selectMaterias.id = "materia";
-				selectMaterias.name =(nombre);
-				if (i == 1) {selectMaterias.required = true;}
-				var def=document.createElement("option");
-				def.value="";
-				def.text="Seleccionar materia";
-				selectMaterias.add(def);
-				// Recorrer los objetos filtrados y agregar opciones al nuevo select
-				objetosFiltrados.forEach(function (objetofiltrsdo) {
-					objetofiltrsdo.forEach(function(objeto){
-					// Verificar si la materia del objeto no está en el select de materias
-					if (!selectMaterias.querySelector('option[value="' + objeto.Clave + '"]')) {
-						// Crear la opción para la materia del objeto
-						var opcion = document.createElement("option");
-						opcion.text = objeto.Materia+" - "+(objeto.Carrera=="Lic. Ciencia de Datos" ? "LCD" : (objeto.Carrera=="Ing. Inteligencia Artificial" ? "IIA" : "ISC"));
-						opcion.value = objeto.Clave;
+							selectMaterias.className = "form-control-dark dropdown-menu dropdown-menu-dark d-block position-static mx-0 border-0 shadow w-220px ";
+							selectMaterias.ariaLabel = "Default select example";
+							selectMaterias.style = "background-color: var(--background-color); color: var( --contrast-dark-color);";
+							selectMaterias.id = "materia";
+							selectMaterias.name = (nombre);
+							if (i == 1) { selectMaterias.required = true; }
+							var def = document.createElement("option");
+							def.value = "";
+							def.text = "Seleccionar materia";
+							selectMaterias.add(def);
+							// Recorrer los objetos filtrados y agregar opciones al nuevo select
+							objetosFiltrados.forEach(function (objetofiltrsdo) {
+								objetofiltrsdo.forEach(function (objeto) {
+									// Verificar si la materia del objeto no está en el select de materias
+									if (!selectMaterias.querySelector('option[value="' + objeto.Clave + '"]')) {
+										// Crear la opción para la materia del objeto
+										var opcion = document.createElement("option");
+										opcion.text = objeto.Materia + " - " + (objeto.Carrera == "Lic. Ciencia de Datos" ? "LCD" : (objeto.Carrera == "Ing. Inteligencia Artificial" ? "IIA" : "ISC"));
+										opcion.value = objeto.Clave;
 
-						// Agregar la opción al nuevo select
-						selectMaterias.add(opcion);
-					}});
-				});
-				// Agregar el nuevo select al documento
-				materia_div.appendChild(selectMaterias);
-				//materia_div.appendChild(materias_extra_label_DOM);
-				//materia_div.appendChild(materias_extra_checkbox_DOM);
-				carta.appendChild(materia_div);
+										// Agregar la opción al nuevo select
+										selectMaterias.add(opcion);
+									}
+								});
+							});
+							// Agregar el nuevo select al documento
+							materia_div.appendChild(selectMaterias);
+							//materia_div.appendChild(materias_extra_label_DOM);
+							//materia_div.appendChild(materias_extra_checkbox_DOM);
+							carta.appendChild(materia_div);
 
-				//carta.appendChild(littlescript);
-			}
+							//carta.appendChild(littlescript);
+						}
 
-		
+
 					}
 				});
 				if (response.error) {
@@ -135,48 +146,48 @@ function dividirString(inputString) {
 			var selectnumber = 2;
 			var restanumber = 0;
 			// Si la casilla de verificación está marcada
-				materias_pp.style.display = "block";
-				materias_pp_label.style.display = "block";
-				materias_ll.style.display = "block";
-				materias_ll_label.style.display = "block";
+			materias_pp.style.display = "block";
+			materias_pp_label.style.display = "block";
+			materias_ll.style.display = "block";
+			materias_ll_label.style.display = "block";
 
-				if (document.querySelectorAll("#materias_pp").length != 0) {
+			if (document.querySelectorAll("#materias_pp").length != 0) {
 
-					materias_pp.addEventListener('click', function () {
-						console.log(selectnumber);
-						// Variable para llevar la cuenta de la cantidad
-						let previous_divs = document.getElementById("materias_div" + selectnumber)
-						
-							previous_divs.style.display = "block";
-							if (selectnumber < 4) { selectnumber = selectnumber + 1; }
-							else { selectnumber = 2; restanumber = 4; }
-							
-						
-					});
-					materias_ll.addEventListener('click', function () {
-						if (restanumber == 4) { selectnumber = 4; restanumber = 0; }
-						console.log(selectnumber);
-						// Variable para llevar la cuenta de la cantidad
-						let previous_divs = document.getElementById("materias_div" + selectnumber)
-						if (previous_divs.style.display == "none" && selectnumber > 2) selectnumber = selectnumber - 1;
-						previous_divs = document.getElementById("materias_div" + selectnumber)
-						
+				materias_pp.addEventListener('click', function () {
+					console.log(selectnumber);
+					// Variable para llevar la cuenta de la cantidad
+					let previous_divs = document.getElementById("materias_div" + selectnumber)
 
-							previous_divs.style.display = "none";
-							if (selectnumber > 2) {
-								selectnumber = selectnumber - 1;
-							}
-							else selectnumber = 2;
-							
-						
-						console.log(selectnumber);
-					});
-				}
-			
-			
+					previous_divs.style.display = "block";
+					if (selectnumber < 4) { selectnumber = selectnumber + 1; }
+					else { selectnumber = 2; restanumber = 4; }
+
+
+				});
+				materias_ll.addEventListener('click', function () {
+					if (restanumber == 4) { selectnumber = 4; restanumber = 0; }
+					console.log(selectnumber);
+					// Variable para llevar la cuenta de la cantidad
+					let previous_divs = document.getElementById("materias_div" + selectnumber)
+					if (previous_divs.style.display == "none" && selectnumber > 2) selectnumber = selectnumber - 1;
+					previous_divs = document.getElementById("materias_div" + selectnumber)
+
+
+					previous_divs.style.display = "none";
+					if (selectnumber > 2) {
+						selectnumber = selectnumber - 1;
+					}
+					else selectnumber = 2;
+
+
+					console.log(selectnumber);
+				});
+			}
+
+
 			selectnumber = 2;
-		}	
+		}
 		);
-		
+
 	})
 	.catch(error => console.error(error));
